062a3388d483c6ea0e1c49bf7d356b20a4c0d16c
saifffff <angryn1nj4@gmail,com>
2022-03-15 23:12:47 +0530

    help command done

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 // Project --> CLI based file organizer 
 
+// Imports
+let helpFunc = require("./commands/help");
+
 // lets take a command first
+
 let inputArr = process.argv.slice(2);
 let command = inputArr[0];
+let path = inputArr[1];
 
 // based on cmd we can call command specific functions
 switch(command){
     case "tree":
-        console.log("command is tree");
+        console.log("command is tree on path - "+path);
         break;
     case "organize":
-        console.log("command is organize");
+        console.log("command is organize on path - "+path);
         break;
     case "help":
-        console.log("command is help");
+        helpFunc.help();
         break;
     default:
         console.log("command not found\nplease try again");
